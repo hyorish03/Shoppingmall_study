7f55aefcff1a1ba20bddebe53e7cf2989cc557f7
hyorish03 <hyorish2001@gmail.com>
2024-01-09 20:03:52 +0900

    Feat: React-Router v6에 맞게 수정 및 fake api 호출

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { getClient } from "./queryClient.ts";
+import { routes } from "./routes.tsx";
+
+const queryClient = getClient();
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,4 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { QueryKeys, fetcher } from "../../queryClient";
+
 const index = () => {
+  const { data } = useQuery({
+    queryKey: [QueryKeys.PRODUCTS],
+    queryFn: () =>
+      fetcher({
+        method: "GET",
+        path: "/products",
+      }),
+  });
+  console.log(data);
+
   return <div>상품목록</div>;
 };
 export default index;
