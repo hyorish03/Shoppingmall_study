916eda6c6edc8f99d5b6e9290dcccc39faf36e5e
hyorish03 <hyorish2001@gmail.com>
2024-01-12 14:57:23 +0900

    Style: 불필요한 코드 삭제

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,10 @@
 // App.tsx
-import { QueryClientProvider } from "react-query";
 import Gnb from "./Components/Gnb";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { getClient } from "./queryClient";
 import { useRoutes } from "react-router-dom";
 import { routes } from "./routes";
 
 const App = () => {
-  const queryClient = getClient();
   const elem = useRoutes(routes);
 
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getClient } from "./queryClient.ts";
-import { routes } from "./routes.tsx";
 import "./scss/index.scss";
 import App from "./app.tsx";
 
 const queryClient = getClient();
-const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
